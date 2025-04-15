2ceb316ee5ec5175b4ebe8898fd0f052183a9a9f
JayWang <1781661231@qq.com>
2025-04-15 23:06:58 +0800

    Jay 2st commit

--- pointcept/datasets/defaults.py
+++ pointcept/datasets/defaults.py
@@ -23,27 +23,29 @@ from .transform import Compose, TRANSFORMS
 
 
 @DATASETS.register_module()
+# 默认数据加载
 class DefaultDataset(Dataset):
+    # 数据集的有效属性列表
     VALID_ASSETS = [
-        "coord",
-        "color",
-        "normal",
-        "strength",
-        "segment",
-        "instance",
-        "pose",
+        "coord", # 坐标
+        "color", # 颜色
+        "normal", # 法线
+        "strength", # 强度
+        "segment", # 语义分割
+        "instance", # 实例分割
+        "pose", # 位姿
     ]
 
     def __init__(
         self,
-        split="train",
-        data_root="data/dataset",
-        transform=None,
-        test_mode=False,
-        test_cfg=None,
-        cache=False,
-        ignore_index=-1,
-        loop=1,
+        split="train", # 数据集划分
+        data_root="data/dataset", # 数据集根目录
+        transform=None, # 数据增强
+        test_mode=False, # 是否测试模式
+        test_cfg=None, # 测试配置
+        cache=False, # 是否使用缓存数据
+        ignore_index=-1, # 忽略的语义索引
+        loop=1, # 数据集循环次数
     ):
         super(DefaultDataset, self).__init__()
         self.data_root = data_root
@@ -59,9 +61,7 @@ class DefaultDataset(Dataset):
 
         if test_mode:
             self.test_voxelize = TRANSFORMS.build(self.test_cfg.voxelize)
-            self.test_crop = (
-                TRANSFORMS.build(self.test_cfg.crop) if self.test_cfg.crop else None
-            )
+            self.test_crop = (TRANSFORMS.build(self.test_cfg.crop) if self.test_cfg.crop else None)
             self.post_transform = Compose(self.test_cfg.post_transform)
             self.aug_transform = [Compose(aug) for aug in self.test_cfg.aug_transform]
 
@@ -73,6 +73,7 @@ class DefaultDataset(Dataset):
             )
         )
 
+    # 获取数据列表
     def get_data_list(self):
         if isinstance(self.split, str):
             split_list = [self.split]
@@ -83,24 +84,28 @@ class DefaultDataset(Dataset):
 
         data_list = []
         for split in split_list:
+            # 若 data_root/split 为文件，则读取该文件
             if os.path.isfile(os.path.join(self.data_root, split)):
                 with open(os.path.join(self.data_root, split)) as f:
                     data_list += [
                         os.path.join(self.data_root, data) for data in json.load(f)
                     ]
+            # 若 data_root/split 为目录，则读取该目录下的所有文件
             else:
                 data_list += glob.glob(os.path.join(self.data_root, split, "*"))
         return data_list
 
     def get_data(self, idx):
-        data_path = self.data_list[idx % len(self.data_list)]
-        name = self.get_data_name(idx)
-        split = self.get_split_name(idx)
+        data_path = self.data_list[idx % len(self.data_list)] # 获取数据路径
+        name = self.get_data_name(idx) # 获取数据名称
+        split = self.get_split_name(idx) # 获取数据划分
+        # 读取缓存数据，依赖SharedArray库
         if self.cache:
             cache_name = f"pointcept-{name}"
             return shared_dict(cache_name)
 
         data_dict = {}
+        # data_path 下应当包含 coord.npy/color.npy/normal.npy 等
         assets = os.listdir(data_path)
         for asset in assets:
             if not asset.endswith(".npy"):
@@ -110,7 +115,7 @@ class DefaultDataset(Dataset):
             data_dict[asset[:-4]] = np.load(os.path.join(data_path, asset))
         data_dict["name"] = name
         data_dict["split"] = split
-
+        # coord、color、normal 为 float32
         if "coord" in data_dict.keys():
             data_dict["coord"] = data_dict["coord"].astype(np.float32)
 
@@ -119,7 +124,8 @@ class DefaultDataset(Dataset):
 
         if "normal" in data_dict.keys():
             data_dict["normal"] = data_dict["normal"].astype(np.float32)
-
+            
+        # segment、instance 为 int32，若不存在用-1填充
         if "segment" in data_dict.keys():
             data_dict["segment"] = data_dict["segment"].reshape([-1]).astype(np.int32)
         else:
@@ -153,18 +159,22 @@ class DefaultDataset(Dataset):
         # load data
         data_dict = self.get_data(idx)
         data_dict = self.transform(data_dict)
+        # 将 sengment 和 name 移动到 result_dict 中
         result_dict = dict(segment=data_dict.pop("segment"), name=data_dict.pop("name"))
+        # 若存在 origin_segment 和 inverse，则将其移动到 result_dict 中
         if "origin_segment" in data_dict:
             assert "inverse" in data_dict
             result_dict["origin_segment"] = data_dict.pop("origin_segment")
             result_dict["inverse"] = data_dict.pop("inverse")
 
         data_dict_list = []
+        # 数据增强，使用深拷贝保证原始数据不被修改
         for aug in self.aug_transform:
             data_dict_list.append(aug(deepcopy(data_dict)))
 
         fragment_list = []
         for data in data_dict_list:
+            # 体素化处理
             if self.test_voxelize is not None:
                 data_part_list = self.test_voxelize(data)
             else:
@@ -178,6 +188,7 @@ class DefaultDataset(Dataset):
                 fragment_list += data_part
 
         for i in range(len(fragment_list)):
+            # 后处理
             fragment_list[i] = self.post_transform(fragment_list[i])
         result_dict["fragment_list"] = fragment_list
         return result_dict
@@ -230,3 +241,4 @@ class ConcatDataset(Dataset):
 
     def __len__(self):
         return len(self.data_list) * self.loop
+
